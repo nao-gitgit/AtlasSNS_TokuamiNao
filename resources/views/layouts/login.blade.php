82650574e4e84a7b5f2f58f2e2cdb1406b6d5009
nao <>
2026-02-04 23:54:42 +0900

    検索フォームを設置 投稿一覧画面とトップページのサイドバーのレイアウトを微調整

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -28,31 +28,58 @@
   <header>
     @include('layouts.navigation')
   </header>
+
   <!-- Page Content -->
   <div id="row">
     <div id="container">
       {{ $slot }}
     </div>
+
     <div id="side-bar">
-      <div id="confirm">
-        <p class="username">{{ Auth::user()->username }}さん</p>
-        <div>
-          <p>フォロー数　　　{{ Auth::user()->follows()->count() }} 名</p>
-        </div>
-        <p class="btn"><a href="{{ url('follows/followlist') }}">フォローリスト</a></p>
-        <div>
-          <p>フォロワー数　　　{{ Auth::user()->followers()->count() }} 名</p>
-        </div>
-        <p class="btn"><a href="{{ url('follows/followerlist') }}">フォロワーリスト</a></p>
-      </div>
-      <p class="btn"><a href="{{ url('users/search') }}">ユーザー検索</a></p>
+  <div id="confirm">
+
+    <p class="username">{{ Auth::user()->username }}さん</p>
+
+    <!-- フォロー数 -->
+    <div class="d-flex justify-content-between align-items-center">
+      <span>フォロー数</span>
+      <span>{{ Auth::user()->follows()->count() }} 人</span>
     </div>
+
+    <div class="d-flex justify-content-end mt-2 mb-4">
+      <a href="{{ url('follows/followlist') }}" class="btn btn-primary">
+        フォローリスト
+      </a>
+    </div>
+
+    <!-- フォロワー数 -->
+    <div class="d-flex justify-content-between align-items-center">
+      <span>フォロワー数</span>
+      <span>{{ Auth::user()->followers()->count() }} 人</span>
+    </div>
+
+    <div class="d-flex justify-content-end mt-2 mb-3">
+      <a href="{{ url('follows/followerlist') }}" class="btn btn-primary">
+        フォロワーリスト
+      </a>
+    </div>
+
+    <!-- 区切り線 -->
+    <hr>
+
+    <!-- ユーザー検索 -->
+    <div class="d-flex justify-content-center mt-3">
+      <a href="{{ url('users/search') }}" class="btn btn-primary">
+        ユーザー検索
+      </a>
+    </div>
+
   </div>
+</div>
+
   <footer>
   </footer>
-  <script src="{{ asset('js/app.js') }}"></script>
-  <script src="JavaScriptファイルのURL"></script>
-  <script src="JavaScriptファイルのURL"></script>
-</body>
 
+  <script src="{{ asset('js/app.js') }}"></cript>
+</body>
 </html>
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,3 +1,20 @@
 <x-login-layout>
 
+    <div class="search-area">
+        <form action="{{ url('users/search') }}" method="GET">
+
+            <input
+                type="text"
+                name="keyword"
+                class="search-input"
+                placeholder="ユーザー名"
+            >
+
+            <button type="submit" class="search-btn">
+                <img src="{{ asset('images/search.png') }}" alt="検索">
+            </button>
+
+        </form>
+    </div>
+
 </x-login-layout>
