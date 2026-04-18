7a3d2451e1d3f4948abe5d8d841fb284ca52eba2
nao <>
2026-04-18 14:15:52 +0900

    新規ユーザー登録画面のレイアウト調整完了

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,35 +1,47 @@
 <x-logout-layout>
 
-{!! Form::open(['url' => 'register']) !!}
+{!! Form::open(['url' => 'register','class'=>'login-form']) !!}
 
-<h2>新規ユーザー登録</h2>
+<h3 class="login-title">新規ユーザー登録</h3>
 
 <!-- エラーメッセージ表示 -->
 @if ($errors->any())
-    <div class="error-massages">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+<div class="error-message">
+  <ul>
+    @foreach ($errors->all() as $error)
+      <li>{{ $error }}</li>
+    @endforeach
+  </ul>
+</div>
 @endif
 
+<div class="form-group">
 {{ Form::label('username','ユーザー名') }}
 {{ Form::text('username',old('username'),['class' => 'input']) }}
+</div>
 
+<div class="form-group">
 {{ Form::label('email','メールアドレス') }}
 {{ Form::email('email',old('email'),['class' => 'input']) }}
+</div>
 
+<div class="form-group">
 {{ Form::label('password','パスワード') }}
 {{ Form::password('password',['class' => 'input']) }}
+</div>
 
+<div class="form-group">
 {{ Form::label('password_confirmation','パスワード確認') }}
 {{ Form::password('password_confirmation',['class' => 'input']) }}
+</div>
 
-{{ Form::submit('新規登録') }}
+<div class="login-btn-area">
+{{ Form::submit('新規登録',['class'=>'login-btn']) }}
+</div>
 
-<p><a href="login">ログイン画面へ戻る</a></p>
+<div class="register-link">
+<a href="/login">ログイン画面へ戻る</a>
+</div>
 
 {!! Form::close() !!}
 
